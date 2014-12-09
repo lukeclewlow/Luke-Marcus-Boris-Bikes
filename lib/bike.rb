27420065276cd498b9e7359d2c44cc76532d898e
Marcus de Wilde <Marcus@Marcuss-MacBook-Pro.local>
2014-12-09 15:53:53 +0000

    bike.rb and spec_bike.rb with fix method and test

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,17 +1,21 @@
 class Bike
 
 	def initialize
-		@broken = false
+		@broken = false #can be changed everywhere in the class
 	end
 
-	def broken?
+
+	def broken? 
 		@broken
 		end
 
-	def break! #exclamation for changing the state of the object
+	def break! #exclamation for changing the state of the class, convention
 		@broken = true
+	end
+
+	def fix!
+		@broken = false
 	end
 
 end
 
-#NEW CHANGE!!!
